2229dac5c159d90bfc489444121a7faba3b7a616
Dom W <media.Dom0_o@protonmail.com>
2021-11-14 23:02:10 -0500

    master password hash feature

--- generate.py
+++ generate.py
@@ -4,14 +4,14 @@ Code for generating a random, secure password
 import random
 import string
 
-def gen():
+def gen(x):
 #generates a password of length 24 with letters, numbers and special chars
     chars = [string.ascii_letters, string.digits, string.punctuation]
     final = ""
-    for i in range(23):
+    for i in range(x):
         rand = random.choice(random.choice(chars))
         final += rand
     return final
 
 if __name__ == "__main__":
-    print(gen())
+    print(gen(10))
--- main.py
+++ main.py
@@ -6,4 +6,29 @@ Main code which will accomplish the following goals:
     4) Allowing access to passwords
     5) Creating and deleting passwords
 '''
+from generate import gen
+import hashlib
 
+def hashed(x):
+#returns sha256 hashed hexadecimal of string input x
+    return hashlib.sha256(bytes(x,"utf-8")).hexdigest()
+
+
+if __name__ == "__main__":
+    print("Welcome to your password vault")
+    dynamic_salt = gen(10)
+
+    master_hash = hashed(input("Select your master password: ")+dynamic_salt)
+    print(master_hash)
+
+    #authenticaion-------------------------------------------------------
+    access_granted = False
+    user_input = hashed(input("Enter your master password: ")+dynamic_salt)
+    print("\nMaster hash:", master_hash)
+    print("Input hash :", user_input)
+    if (user_input == master_hash):
+        print("\nACCESS_GRANTED")
+        access_granted = True
+    else:
+        print("\nACCESS_DENIED")
+    #--------------------------------------------------------------------
